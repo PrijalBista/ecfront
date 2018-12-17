b54885f9c552d4d3ff7a27aef16a474c9ad066c1
PrijalBista <pri1.zzal@gmail.com>
2018-12-17 12:55:19 +0545

    carousel

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <navbar></navbar>
+    <carousel></carousel>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
  import Navbar from './Components/Navbar.vue'
+ import Carousel from './Components/Carousel.vue'
 export default {
   name: 'app',
-  components:{'navbar':Navbar},
+  components:{'navbar':Navbar,'carousel':Carousel},
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
---
+++ src/Components/Carousel.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="carouselExampleIndicators" class="carousel slide container-fluid" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <a href="/product/1">
+	      	<img class="d-block w-100" src="https://picsum.photos/970/250/?image=1" alt="First slide">
+	      	<div class="carousel-caption d-none d-md-block">
+	      	    <h3>Amazing Offer</h3>
+	      	    <p>Some info about product</p>
+	      	</div>
+	      </a>
+	    </div>
+	    <div class="carousel-item">
+	    	<a href="">
+	      		<img class="d-block w-100" src="https://picsum.photos/970/250/?image=2" alt="Second slide">	
+	    	</a>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="https://picsum.photos/970/250?image=3" alt="Third slide">
+	      <div class="carousel-caption d-none d-md-block">
+	          <h5>New Deal</h5>
+	          <p>Some info about product</p>
+	      </div>
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+</template>
+
+<style>
+	.carousel img{
+	/*	width:auto;
+		height: auto;
+		max-width: 80%;
+		margin:auto;*/
+		max-height:550px;
+	}
+</style>
