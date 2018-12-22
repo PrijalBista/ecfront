bf35b27f82e1dfc48665581c834f89f9b97dd1c3
PrijalBista <pri1.zzal@gmail.com>
2018-12-22 16:49:14 +0545

    restructure finalize

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 
   <div id="app">
     <navbar></navbar>
-    <carousel></carousel>
     <router-view></router-view>
   </div>
 </template>
@@ -13,27 +12,5 @@ import Carousel from './components/Carousel.vue'
 export default {
   name: 'app',
   components:{'navbar':Navbar,'carousel':Carousel},
-  data() {
-    return {
-        auth:{
-            client_id: 2,
-            client_secret: 'lfSNsQpVDMcf5sugWkG6GaINVTZ3M7PsxiAQhhcY',
-            grant_type: 'password',
-            username: "judah05@example.net",
-            password: "secret",
-            scope: "place-orders"
-        }
-    }
-  },
-
-  methods:{
-    login(){
-        this.$store.dispatch('asyncSetAuthUser', this.auth);
-    },
-
-    logout(){
-        this.$store.dispatch('asyncUnsetAuthUser');
-    }
-  }
 }
 </script>
